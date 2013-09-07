da5f26143052528192dea295b48a855a22dba95b
John Suykerbuyk <john@suykerbuyk.org>
2013-09-07 14:01:36 -0600

    Modify build scripts to isolate configuring and building of Debug & Release ./clean.sh ./configure.sh ./make.Debug ./make.Release

--- configure.sh
+++ configure.sh
@@ -2,6 +2,6 @@
 mkdir -p ./bld/Debug/prj >/dev/null
 mkdir -p ./bld/Release/prj >/dev/null
 cd ./bld/Debug/prj
-cmake ../../.. -DCMAKE_BUILD_TYPE=Debug && make
-#cd ../Release
-#cmake ../../.. -DCMAKE_BUILD_TYPE=Release && make 
+cmake ../../.. -DCMAKE_BUILD_TYPE=Debug
+cd ../Release
+cmake ../../.. -DCMAKE_BUILD_TYPE=Release
